acedf8d37ee4a11777af93b45a48a0c006d5269b
cmlegault <chris.legault@noaa.gov>
2019-03-29 15:55:30 -0400

    still searching for param.list error

--- jitter_asap.R
+++ jitter_asap.R
@@ -26,18 +26,18 @@ jitter_asap <- function(in.pin, param.list){
       if (nrows == 1){
         nvals <- length(out.pin$dat[[i]][[1]])
         for (ival in 1:nvals){
-          out.pin$dat[[i]][[ival]] <- runif(1, 
-                                            param.list$lowerbound[[i]][[ival]],
-                                            param.list$upperbound[[i]][[ival]])
+          out.pin$dat[[i]][ival] <- runif(1, 
+                                            param.list$lowerbound[[i]][ival],
+                                            param.list$upperbound[[i]][ival])
         }
 #        out.pin$dat[[i]][[1]] <- runif(nvals, param.list$lowerbound[i], param.list$upperbound[i])
       }else{
         for (irow in 1:nrows){
           nvals <- length(out.pin$dat[[i]][[irow]])
           for (ival in 1:nvals){
-            out.pin$dat[[i]][[irow]][[ival]] <- runif(1, 
-                        param.list$lowerbound[[i]][[irow]][[ival]],
-                        param.list$upperbound[[i]][[irow]][[ival]])
+            out.pin$dat[[i]][[irow]][ival] <- runif(1, 
+                        param.list$lowerbound[[i]][[irow]][ival],
+                        param.list$upperbound[[i]][[irow]][ival])
           }
 #          out.pin$dat[[i]][[irow]] <- runif(nvals, param.list$lowerbound[i], param.list$upperbound[i])
         }
