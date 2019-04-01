301518f1285162e46e6d85d8e214c3ddbd8f8cdb
cmlegault <chris.legault@noaa.gov>
2019-04-01 15:52:37 -0400

    remove unneeded functions

--- jitter_asap.R
+++ jitter_asap.R
@@ -1,19 +1,3 @@
-get_rand_mfac <- function(asap.dat, repname, mfac){
-  asap.adj <- asap.dat
-  init_val <- asap.dat$dat[names(asap.dat$dat) == repname][[1]]
-  adj_val <- runif(length(init_val), init_val / mfac, init_val * mfac)
-  asap.adj$dat[names(asap.adj$dat) == repname][[1]] <- adj_val
-  return(asap.adj)
-}
-
-get_rand_bound <- function(asap.dat, repname, lowbound, highbound){
-  asap.adj <- asap.dat
-  init_val <- asap.dat$dat[names(asap.dat$dat) == repname][[1]]
-  adj_val <- runif(length(init_val), lowbound, highbound)
-  asap.adj$dat[names(asap.adj$dat) == repname][[1]] <- adj_val
-  return(asap.adj)
-}
-
 # jitter_asap
 # changes .pin file to use random restarts
 
