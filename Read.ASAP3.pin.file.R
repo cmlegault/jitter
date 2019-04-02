8e6920ed1e22ce7b460375ca56ddc6fbd582f097
cmlegault <chris.legault@noaa.gov>
2019-04-02 13:53:28 -0400

    remove leading space after reading in pin file

--- Read.ASAP3.pin.file.R
+++ Read.ASAP3.pin.file.R
@@ -8,6 +8,13 @@ read.asap3.pin.file <- function(pinf){
   dat.start <- com.ind[c(which(diff(com.ind)>1), length(com.ind))]
   comments <- char.lines[dat.start]
   
+  # drop leading space in data lines
+  for (i in 1:length(char.lines)){
+    if (substring(char.lines[i], 1, 1) == " "){
+      char.lines[i] <- substring(char.lines[i], 2, nchar(char.lines[i]))
+    }
+  }
+  
   dat <- list()
   np <- length(comments)
   ind <- 0
@@ -23,39 +30,40 @@ read.asap3.pin.file <- function(pinf){
   # log Fmult year 1 (one value for each fleet)
   ind <- ind + 1
   dat[[ind]] <- list()
-  nfleets <- length(strsplit(char.lines[dat.start[ind] + 1], " ")[[1]]) - 1
+  nfleets <- length(strsplit(char.lines[dat.start[ind] + 1], " ")[[1]])
   dat[[ind]][[1]] <- scan(pinf, what = double(), skip = dat.start[ind], n = nfleets)
   
   # log Fmult devs (one row for each fleet, each row nyears-1 long)
   ind <- ind + 1
   dat[[ind]] <- list()
-  nyears <- length(strsplit(char.lines[dat.start[ind] + 1], " ")[[1]])
+  ndevs <- length(strsplit(char.lines[dat.start[ind] + 1], " ")[[1]])
   for (ifleet in 1:nfleets){
-    dat[[ind]][[ifleet]] <- scan(pinf, what = double(), skip = dat.start[ind] + ifleet - 1, n = nyears - 1)
+    dat[[ind]][[ifleet]] <- scan(pinf, what = double(), skip = dat.start[ind] + ifleet - 1, n = ndevs)
   }
   
   # log recruit devs (one value for each year)
   ind <- ind + 1
   dat[[ind]] <- list()
+  nyears <- length(strsplit(char.lines[dat.start[ind] + 1], " ")[[1]])
   dat[[ind]][[1]] <- scan(pinf, what = double(), skip = dat.start[ind], n = nyears)
   
   # log N year 1 devs (one value for each age except age 1 (# values = nages - 1))
   ind <- ind + 1
   dat[[ind]] <- list()
-  nages <- length(strsplit(char.lines[dat.start[ind] + 1], " ")[[1]]) # note no minus one here
-  dat[[ind]][[1]] <- scan(pinf, what = double(), skip = dat.start[ind], n = nages - 1)
+  nagesm1 <- length(strsplit(char.lines[dat.start[ind] + 1], " ")[[1]]) 
+  dat[[ind]][[1]] <- scan(pinf, what = double(), skip = dat.start[ind], n = nagesm1)
   
   # log q year 1 (one value for each index)
   ind <- ind + 1
   dat[[ind]] <- list()
-  ninds <- length(strsplit(char.lines[dat.start[ind] + 1], " ")[[1]]) - 1
+  ninds <- length(strsplit(char.lines[dat.start[ind] + 1], " ")[[1]])
   dat[[ind]][[1]] <- scan(pinf, what = double(), skip = dat.start[ind], n = ninds)
   
   # log q devs (one row for each index, each row depends on number of obs in that index, so varies)
   ind <- ind + 1
   dat[[ind]] <- list()
   for (ii in 1:ninds){
-    nobs <- length(strsplit(char.lines[dat.start[ind] + ii], " ")[[1]]) - 1
+    nobs <- length(strsplit(char.lines[dat.start[ind] + ii], " ")[[1]])
     dat[[ind]][[ii]] <- scan(pinf, what = double(), skip = dat.start[ind] + ii - 1, n = nobs)
   }
   
