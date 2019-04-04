40504b523216d18ad8d3d0850dadac09d02fc5e7
cmlegault <chris.legault@noaa.gov>
2019-04-04 16:04:14 -0400

    fix od

--- plot_jitter.R
+++ plot_jitter.R
@@ -7,7 +7,7 @@ plot_jitter <- function(reslist, asap.rdat, save.plots, od, plotf){
   plot(1:length(reslist$objfxn), reslist$objfxn, xlab = "Realization", ylab = "Objective Function")
     abline(h = asap.rdat$like$lk.total, col="red")
   
-  if (save.plots==TRUE) savePlot(paste0(wd, "jitter_objfxn.", plotf), type=plotf)
+  if (save.plots==TRUE) savePlot(paste0(od, "jitter_objfxn.", plotf), type=plotf)
 
     # put these in separate function and make optional
     # #g <- ggplot(ssbdf, aes(x=Year, y=SSB, color=as.factor(jitter))) +
