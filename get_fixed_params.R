92042c4191f5cdfe266714a1f35e2f3594daced9
cmlegault <chris.legault@noaa.gov>
2019-04-08 07:57:22 -0400

    change asap.dat to asap for consistency with ASAPplots

--- get_fixed_params.R
+++ get_fixed_params.R
@@ -1,13 +1,14 @@
-# get_fixed_params
+# get_fixed_params from asap.rdat object
+# returns data frame of parameters as either "fixed" or "estimated"
 
-get_fixed_params <- function(asap.dat){
-  names(asap.dat$dat)
+get_fixed_params <- function(asap){
+  names(asap$dat)
   counter <- 0
   res <- data.frame()
-  sel_block_option <- asap.dat$dat$sel_block_option
-  sel_ini <- asap.dat$dat$sel_ini
-  nages <- asap.dat$dat$n_ages
-  for (iblock in 1:asap.dat$dat$n_fleet_sel_blocks){
+  sel_block_option <- asap$dat$sel_block_option
+  sel_ini <- asap$dat$sel_ini
+  nages <- asap$dat$n_ages
+  for (iblock in 1:asap$dat$n_fleet_sel_blocks){
     sel_ini_block <- sel_ini[[iblock]]
     if (sel_block_option[iblock] == 1){  # by age
       counter <- counter + 1:nages
@@ -29,18 +30,18 @@ get_fixed_params <- function(asap.dat){
   
   counter <- max(counter) + 1:6 
   temp <- rep("estimated", 6)
-  if (asap.dat$dat$phase_F1 <= 0)       temp[1] <- "fixed"
-  if (asap.dat$dat$phase_F_devs <= 0)   temp[2] <- "fixed"
-  if (asap.dat$dat$phase_rec_devs <= 0) temp[3] <- "fixed"
-  if (asap.dat$dat$phase_N1_devs <= 0)  temp[4] <- "fixed"
-  if (asap.dat$dat$phase_q <= 0)        temp[5] <- "fixed"
-  if (asap.dat$dat$phase_q_devs <= 0)   temp[6] <- "fixed"
+  if (asap$dat$phase_F1 <= 0)       temp[1] <- "fixed"
+  if (asap$dat$phase_F_devs <= 0)   temp[2] <- "fixed"
+  if (asap$dat$phase_rec_devs <= 0) temp[3] <- "fixed"
+  if (asap$dat$phase_N1_devs <= 0)  temp[4] <- "fixed"
+  if (asap$dat$phase_q <= 0)        temp[5] <- "fixed"
+  if (asap$dat$phase_q_devs <= 0)   temp[6] <- "fixed"
   res <- rbind(res, data.frame(counter=counter, param=temp))
   counter <- max(counter)
   
-  index_sel_option <- asap.dat$dat$index_sel_option
-  index_sel_ini <- asap.dat$dat$index_sel_ini
-  for (ind in 1:asap.dat$dat$n_indices){
+  index_sel_option <- asap$dat$index_sel_option
+  index_sel_ini <- asap$dat$index_sel_ini
+  for (ind in 1:asap$dat$n_indices){
     index_ini <- index_sel_ini[[ind]]
     if (index_sel_option[ind] == 1){  # by age
       counter <- counter + 1:nages
@@ -62,8 +63,8 @@ get_fixed_params <- function(asap.dat){
   
   counter <- max(counter) + 1:2 
   temp <- rep("estimated", 2)
-  if (asap.dat$dat$phase_SR_scalar <= 0) temp[1] <- "fixed"
-  if (asap.dat$dat$phase_steepness <= 0) temp[2] <- "fixed"
+  if (asap$dat$phase_SR_scalar <= 0) temp[1] <- "fixed"
+  if (asap$dat$phase_steepness <= 0) temp[2] <- "fixed"
   res <- rbind(res, data.frame(counter=counter, param=temp))
   
   return(res)
