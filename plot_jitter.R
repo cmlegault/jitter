856617502f9521dc3692d5a3221a6eef74a16a38
cmlegault <chris.legault@noaa.gov>
2019-04-11 15:28:52 -0400

    add ymaxlimit

--- plot_jitter.R
+++ plot_jitter.R
@@ -6,31 +6,42 @@
 #' @param od directory where plot will be saved
 #' @param plotf format for individual plots 
 #' @param showtitle option to add title for plot providing number of realizations better than original, equal to original, and not converged
+#' @param ymaxlimit replace any objfxn value greater than this value with this value in plot (default = NULL means no replacement of any values)
 #' @export
 
-PlotJitter <- function(reslist, save.plots, od, plotf, showtitle){
+PlotJitter <- function(reslist, save.plots, od, plotf, showtitle, ymaxlimit=NULL){
   
   nna <- length(reslist$objfxn[is.na(reslist$objfxn)])
   neq <- length(which(reslist$objfxn == reslist$orig_objfxn))
   loreals <- which(reslist$objfxn < reslist$orig_objfxn)
   
   if (length(loreals) == 1){
-    maintitle <- paste0("Realization ", loreals, " is less than original")
+    title1 <- paste0("Realization ", loreals, " is less than original")
   }else if (length(loreals > 1)){
-    maintitle <- paste0(length(loreals), " realizations are less than original")
+    title1 <- paste0(length(loreals), " realizations are less than original")
   }else{
-    maintitle <- "No realizations had objective function lower than original"
+    title1 <- "No realizations had objective function lower than original"
   }
   
-  subtitle <- paste0(nna, " realizations did not converge and ", neq, " had same objfxn as orig")
-  maintitle <- paste0(maintitle, "\n", subtitle)
+  title2 <- paste0(nna, " realizations did not converge and ", neq, " had same objfxn as orig")
+  maintitle <- paste0(title1, "\n", title2)
+
+  showreplaced <- FALSE
+  if (!is.null(ymaxlimit)){
+    replacevals <- which(reslist$objfxn > ymaxlimit)
+    nreplacevals <- length(replacevals)
+    if (nreplacevals > 0){
+      showreplaced <- TRUE
+      reslist$objfxn[replacevals] <- ymaxlimit
+    }
+  }
   
   windows(record = TRUE)
   
   plot(1:length(reslist$objfxn), reslist$objfxn, xlab = "Realization", ylab = "Objective Function")
     abline(h = reslist$orig_objfxn, col="red")
     if (showtitle == TRUE) title(main = maintitle)
-  
+    if (showreplaced == TRUE) points(replacevals, rep(ymaxlimit, nreplacevals), pch=17)
   if (save.plots==TRUE) savePlot(paste0(od, "jitter_objfxn.", plotf), type=plotf)
 
   return()
